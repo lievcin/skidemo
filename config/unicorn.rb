216b05428643eead1cbf1e4e32f6c1b9b0d5d204
Liev <liev.garcia@gmail.com>
2012-05-27 18:09:13 +0100

    first commit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/skimanager/current"
+root = "/home/deployer/apps/skidemo/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.skimanager.sock"
+listen "/tmp/unicorn.skidemo.sock"
 worker_processes 2
 timeout 30
